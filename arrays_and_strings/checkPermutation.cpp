898ee7c3f111eaeafc75032063373e256a928de7
Thomas Ngo <tngo0508@gmail.com>
2018-08-09 11:34:35 -0700

    check permutation by character count

--- arrays_and_strings/checkPermutation.cpp
+++ arrays_and_strings/checkPermutation.cpp
@@ -17,6 +17,29 @@ bool checkPermuationSort(string s, string t) {
     return true;
 };
 
+bool checkPermByCharCount(string s, string t) {
+    if (s.length() != t.length()) { // permuation must be the same length
+        return false;
+    }
+
+    // mapping each character to its frequency in hash table
+    // increment through the first thing, then decrement through the second
+    // string. If strings are permutation, then array will be all zeroes at
+    // the end.
+    int hashTable[128] = {0}; // assuming that we are using ASCII
+    for (size_t i = 0; i < s.length(); i++) {
+        int x = s[i];
+        hashTable[x]++;
+    }
+
+    for (size_t i = 0; i < t.length(); i++) {
+        int x = t[i];
+        hashTable[x]--;
+        if (hashTable[x] < 0) return false; // strings do not have same characters
+    }
+    return true;
+};
+
 int main() {
     string str1 = "hello";
     string str2 = "olhle";
@@ -30,9 +53,20 @@ int main() {
         cout << str1 << " and " << str2 << " are NOT permutation of each other\n";
     }
     if (checkPermuationSort(str3, str4)) {
-        cout << str1 << " and " << str3 << " are permutation of each other\n";
+        cout << str3 << " and " << str4 << " are permutation of each other\n";
     } else {
-        cout << str1 << " and " << str4 << " are NOT permutation of each other\n";
+        cout << str3 << " and " << str4 << " are NOT permutation of each other\n";
+    }
+    cout << "Using character count\n";
+    if (checkPermByCharCount(str1, str2)) {
+        cout << str1 << " and " << str2 << " are permutation of each other\n";
+    } else {
+        cout << str1 << " and " << str2 << " are NOT permutation of each other\n";
+    }
+    if (checkPermByCharCount(str3, str4)) {
+        cout << str3 << " and " << str4 << " are permutation of each other\n";
+    } else {
+        cout << str3 << " and " << str4 << " are NOT permutation of each other\n";
     }
 
     cout << endl    ;
