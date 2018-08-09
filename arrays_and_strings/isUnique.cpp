73cda05fa595ed38b23c7a4eae8175d7800fdc67
Thomas Ngo <tngo0508@gmail.com>
2018-08-08 23:40:06 -0700

    optimize and clean code

--- arrays_and_strings/isUnique.cpp
+++ arrays_and_strings/isUnique.cpp
@@ -4,23 +4,29 @@
 #include <string>
 using namespace std;
 
+bool isUniqueChars(string str) {
+    // the string length cannot exceed the number of the number of unique
+    // characters in the alphabet in ASCII
+    if (str.length() > 128) return false;
+
+    bool char_set[128] = {false};
+    for (size_t i = 0; i < str.length(); i++) {
+        int val = str[i];
+
+        // already found this char in string
+        if (char_set[val]) return false;
+
+        char_set[val] = true;
+    }
+    return true;
+}
+
 int main () {
     cout << "Enter a string: ";
     string str;
     getline(cin, str);
-    int idx = 0;
-    bool isUnique = true;
-    bool hashTable[] = {false};
-    for (size_t i = 0; i < str.length(); i++) {
-        idx = str[i];
-        if (hashTable[idx] == false) hashTable[idx] = true;
-        else {
-            cout << "not unique";
-            isUnique = false;
-            break;
-        }
-    }
-    if (isUnique) cout << "unique";
+    if (isUniqueChars(str)) cout << "unique";
+    else cout << "not unique";
 
 
     cout << endl;
