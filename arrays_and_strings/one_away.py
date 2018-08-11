d11805e830a7f939ddbe4b3e57e5578db2321005
Thomas Ngo <tngo0508@gmail.com>
2018-08-11 13:17:38 -0700

    one away problem

---
+++ arrays_and_strings/one_away.py
@@ -0,0 +1,45 @@
+import unittest
+
+def one_away(s1, s2):
+    pass
+
+def one_edit_replace(s1, s2):
+    pass
+
+def one_edit_insert(s1, s2):
+    pass
+
+class Test(unittest.TestCase):
+    """docstring for Test."""
+    data = [
+        ('pale', 'ple', True),
+        ('pales', 'pale', True),
+        ('pale', 'bale', True),
+        ('paleabc', 'pleabc', True),
+        ('pale', 'ble', False),
+        ('a', 'b', True),
+        ('', 'd', True),
+        ('d', 'de', True),
+        ('pale', 'pale', True),
+        ('pale', 'ple', True),
+        ('ple', 'pale', True),
+        ('pale', 'bale', True),
+        ('pale', 'bake', False),
+        ('pale', 'pse', False),
+        ('ples', 'pales', True),
+        ('pale', 'pas', False),
+        ('pas', 'pale', False),
+        ('pale', 'pkle', True),
+        ('pkle', 'pable', False),
+        ('pal', 'palks', False),
+        ('palks', 'pal', False)
+    ]
+
+    def test_one_away(self):
+        """testing"""
+        for [test_s1, test_s2, expected] in self.data:
+            actual = one_away(test_s1, test_s2)
+            self.assertEqual(actual, expected)
+
+if __name__ == '__main__':
+    unittest.main()
