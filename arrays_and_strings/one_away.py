97a6ccb9de7b4fd0bebf2a3dd566463feac7643d
Thomas Ngo <tngo0508@gmail.com>
2018-08-11 13:56:23 -0700

    add solution

--- arrays_and_strings/one_away.py
+++ arrays_and_strings/one_away.py
@@ -1,13 +1,42 @@
 import unittest
 
-def one_away(s1, s2):
-    pass
+def one_away(str1, str2):
+    """check if string is one edit away
+    The lengths of the strings will indicate which of these you need to check
+    """
+    if len(str1) == len(str2):
+        return one_edit_replace(str1, str2)
+    elif len(str1) + 1 == len(str2):
+        return one_edit_insert(str1, str2)
+    elif len(str1) - 1 == len(str2):
+        return one_edit_insert(str2, str1)
+    return False
 
-def one_edit_replace(s1, s2):
-    pass
+def one_edit_replace(str1, str2):
+    """check if string is one edit away by replacing a character"""
+    found = False
+    for char1, char2 in zip(str1, str2):
+        if char1 != char2:
+            if found:
+                return False
+            found = True
+    return True
 
-def one_edit_insert(s1, s2):
-    pass
+def one_edit_insert(str1, str2):
+    """check if string is one edit away by inserting/removing a character"""
+    found = False
+    idx1, idx2 = 0, 0
+    while idx1 < len(str1) and idx2 < len(str2):
+        if str1[idx1] != str2[idx2]:
+            if found:
+                return False
+            found = True
+            idx2 += 1
+        else:
+            idx1 += 1
+            idx2 += 1
+    return True
+
 
 class Test(unittest.TestCase):
     """docstring for Test."""
