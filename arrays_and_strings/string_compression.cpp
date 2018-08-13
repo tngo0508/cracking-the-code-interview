2596d2aed698d8a5b0be575fe00a31c6121a90ff
Thomas Ngo <tngo0508@gmail.com>
2018-08-13 09:48:25 -0700

    c++ solution for string compression

---
+++ arrays_and_strings/string_compression.cpp
@@ -0,0 +1,44 @@
+// string compression problem
+#include <iostream>
+#include <string>
+
+std::string compress(std::string str) {
+    size_t original_length = str.length();
+    if (original_length < 2) {
+        return str;
+    }
+    std::string out{""};
+    int count = 1;
+    for (size_t i = 0; i < original_length; i++) {
+        if (str[i - 1] == str[i]) {
+            count++;
+        } else {
+            out += str[i - 1];
+            out += std::to_string(count);
+            count = 1;
+        }
+        if (out.length() >= original_length) {
+            return str;
+        }
+    }
+    out += str[original_length - 1];
+    out += std::to_string(count);
+    if (out.length() >= original_length) {
+        return str;
+    }
+    return out;
+}
+
+int main() {
+    std::string str, out;
+    std::cout << "Enter a string: \n";
+    std::cin >> str;
+    out = compress(str);
+    if (str.compare(out)) {
+        std::cout << str << " can be compresed to " << out << '\n';
+    } else {
+        std::cout << str << " can not be compressed" << '\n';
+    }
+    std::cout << std::endl;
+    return 0;
+}
