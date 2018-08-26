0f0bc45978a8ea20dfafcab8aa1fcd8ce26b8859
thomas <tngo0508@gmail.com>
2018-08-25 22:39:38 -0700

    reduce the number of unclassified elements

--- arrays_and_strings/python/dutch_flag_partition.py
+++ arrays_and_strings/python/dutch_flag_partition.py
@@ -40,6 +40,28 @@ def dutch_flag_partition2(pivot_index, A):
     return A
 
 
+def dutch_flag_partition3(pivot_index, A):
+    pivot = A[pivot_index]
+    # Keep the following invariants during partitioning
+    # bottom group: A[:smaller]
+    # middle group: A[smaller:equal]
+    # unclassified group: A[equal:larger]
+    # top group: A[larger:]
+    smaller, equal, larger = 0, 0, len(A)
+    # Keep iterating as long as there is an unclassified element.
+    while equal < larger:
+        # A[equal] is the incoming unclassified elements.
+        if A[equal] < pivot:
+            A[smaller], A[equal] = A[equal], A[smaller]
+            smaller, equal = smaller + 1, equal + 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:  # A[equal] > pivot.
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+    return A
+
+
 class Test(unittest.TestCase):
     """Test case"""
     data1 = [
@@ -49,6 +71,11 @@ class Test(unittest.TestCase):
 
     data2 = [
         ([0, 1, 2, 0, 2, 1, 1], 3, [0, 0, 1, 2, 2, 1, 1]),
+        ([0, 1, 2, 0, 2, 1, 1], 2, [0, 1, 0, 1, 1, 2, 2]),
+    ]
+
+    data3 = [
+        ([0, 1, 2, 0, 2, 1, 1], 3, [0, 0, 2, 2, 1, 1, 1]),
         ([0, 1, 2, 0, 2, 1, 1], 2, [0, 1, 0, 1, 1, 2, 2]),
     ]
 
@@ -62,6 +89,12 @@ class Test(unittest.TestCase):
         """testing"""
         for [test_arr, pivot_index, expected] in self.data2:
             actual = dutch_flag_partition2(pivot_index, test_arr)
+            self.assertEqual(actual, expected)
+
+    def test_dutch_flag_partition3(self):
+        """testing"""
+        for [test_arr, pivot_index, expected] in self.data3:
+            actual = dutch_flag_partition3(pivot_index, test_arr)
             self.assertEqual(actual, expected)
 
 
