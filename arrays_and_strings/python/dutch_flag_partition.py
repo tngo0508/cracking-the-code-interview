997b6aa307cfa9f6ffc3b345ceb48b547a719510
thomas <tngo0508@gmail.com>
2018-08-25 22:32:06 -0700

    improve dutch flag partition to O(n)

--- arrays_and_strings/python/dutch_flag_partition.py
+++ arrays_and_strings/python/dutch_flag_partition.py
@@ -2,7 +2,7 @@ import unittest
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index, A):
+def dutch_flag_partition1(pivot_index, A):
     pivot = A[pivot_index]
     # First pass: group elements smaller than pivot.
     for i in range(len(A)):
@@ -23,17 +23,45 @@ def dutch_flag_partition(pivot_index, A):
     return A
 
 
+def dutch_flag_partition2(pivot_index, A):
+    pivot = A[pivot_index]
+    # First pass: group elements smaller than pivot
+    smaller = 0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller += 1
+    # Second pass: group elements larger than pivot.
+    larger = len(A) - 1
+    for i in reversed(range(len(A))):
+        if A[i] > pivot:
+            A[i], A[larger] = A[larger], A[i]
+            larger -= 1
+    return A
+
+
 class Test(unittest.TestCase):
     """Test case"""
-    data = [
+    data1 = [
         ([0, 1, 2, 0, 2, 1, 1], 3, [0, 0, 1, 1, 2, 2, 1]),
         ([0, 1, 2, 0, 2, 1, 1], 2, [1, 0, 0, 1, 1, 2, 2]),
     ]
 
-    def test_dutch_flag_partition(self):
+    data2 = [
+        ([0, 1, 2, 0, 2, 1, 1], 3, [0, 0, 1, 2, 2, 1, 1]),
+        ([0, 1, 2, 0, 2, 1, 1], 2, [0, 1, 0, 1, 1, 2, 2]),
+    ]
+
+    def test_dutch_flag_partition1(self):
         """testing"""
-        for [test_arr, pivot_index, expected] in self.data:
-            actual = dutch_flag_partition(pivot_index, test_arr)
+        for [test_arr, pivot_index, expected] in self.data1:
+            actual = dutch_flag_partition1(pivot_index, test_arr)
+            self.assertEqual(actual, expected)
+
+    def test_dutch_flag_partition2(self):
+        """testing"""
+        for [test_arr, pivot_index, expected] in self.data2:
+            actual = dutch_flag_partition2(pivot_index, test_arr)
             self.assertEqual(actual, expected)
 
 
